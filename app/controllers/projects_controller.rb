622a31281a1c6d57f57d1124d5c95d874d436ccd
mindziukasss <mindziukasss@gmail.com>
2016-10-18 18:21:49 +0300

    Setup & styled project functionality, friendly_id for projects and add disqus, will_paginate

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,58 @@
+class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
+		
+
+	def index
+		@projects = Project.all.order("created_at desc").paginate(page: params[:page], per_page: 2)
+		
+	end
+
+	def new
+		@project = Project.new
+
+	end
+
+	def create
+		@project = Project.new project_params
+
+		if @project.save
+			redirect_to @project, notice: "That project was successfull saved!"
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def  update
+		if @project.update project_params
+		 redirect_to @project, notice: "That project was successfull updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+		
+	end
+
+private
+
+	def find_project
+		@project = Project.friendly.find(params[:id])
+		
+	end
+
+	def project_params
+		params.require(:project).permit(:title, :description, :link, :slug)
+	end
+
+end
