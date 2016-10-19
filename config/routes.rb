f3bdcbb1739817bd26d4b991c97d2bd36dd968e5
mindziukasss <mindziukasss@gmail.com>
2016-10-19 14:57:02 +0300

    Remove devise signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 	resources :contacts, only: [:new, :create]
 	get 'welcome/index'
 	root 'welcome#index'
+
+	get '*path' => redirect('/')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
